d891eef412bcb56a0f5a4afcbd72670e3e01f344
baba-s <shotababa1206@gmail.com>
2022-11-01 19:43:58 +0900

    add

---
+++ Editor/SupportedFormats.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace MyProject
+{
+    /// <summary>
+    /// サポートする形式を管理するクラス
+    /// </summary>
+    public static class SupportedFormats
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// AudioClip の場合 true を返します
+        /// </summary>
+        public static bool IsAudioClipPath( string path )
+        {
+            var extension = Path.GetExtension( path );
+
+            return IsAudioClipExtension( extension );
+        }
+
+        /// <summary>
+        /// AudioClip の場合 true を返します
+        /// </summary>
+        public static bool IsAudioClipExtension( string extension )
+        {
+            return
+                extension is
+                    ".mp3" or
+                    ".ogg" or
+                    ".wav" or
+                    ".aiff" or
+                    ".aif" or
+                    ".mod" or
+                    ".it" or
+                    ".s3m" or
+                    ".xm"
+                ;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SupportedFormats
+# Kogane Supported Formats
+
+指定されたパスや拡張子が Unity がサポートする形式かどうか確認できるクラス
